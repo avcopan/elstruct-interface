cb3c94d6b5ab75030fa166dbc8cb872b8c61a940
avcopan <avcopan@gmail.com>
2018-11-19 15:21:55 -0600

    Adds sample molpro runner

--- tmp/g09/water.py
+++ tmp/g09/water.py
@@ -14,9 +14,20 @@ COORDS = ((0.000000000000,  0.000000000000, -0.143225816552),
           (0.000000000000,  1.638036840407,  1.136548822547),
           (0.000000000000, -1.638036840407,  1.136548822547))
 
-# make the caller
+fill_vals = {
+    'hostnodes': "b440",
+    'input': "input.dat",
+    'output': "output.dat"
+}
+TEMP_STR = Template(filename='g09e.mako').render(**fill_vals)
+
 def runner():
-    subprocess.check_call(["g09", "<", "input.dat", ">", "output.dat"], shell=True)
+    temp_fle = open('run.sh', 'w')
+    temp_fle.write(TEMP_STR)
+    temp_fle.close()
+
+    subprocess.check_call(["chmod", "+x", 'run.sh'])
+    subprocess.check_call(['./run.sh'])
 
 ENERGY = interfaces.energy(
         prog=PROG, runner=runner, theory=THEORY, basis=BASIS, labels=LABELS,
